7c105f9e91f4e12eb1a2acf3fe259bf546a93bf2
sam <stuart.mclaren@hpe.com>
2024-11-18 14:24:47 +0000

    Add hypervisor cluster delete
    Now that the server API supports hypervisor cluster delete, we
    can add support in the Terraform provider.

--- internal/simulator/hypervisorcluster_delete.go
+++ internal/simulator/hypervisorcluster_delete.go
@@ -8,19 +8,22 @@ import (
 	"github.com/h2non/gock"
 )
 
-//go:embed fixtures/hypervisorclusters/create/asyncfake.json
-var clusterAsync string
+//go:embed fixtures/hypervisorclusters/delete/async1.json
+var deleteTaskOne string
 
-//go:embed fixtures/hypervisorclusters/create/getfake.json
-var clusterGet string
+//go:embed fixtures/hypervisorclusters/delete/async2.json
+var deleteTaskTwo string
 
-func hypervisorClusterCreate() {
-	taskID := "99992745-532b-404c-860c-20a6571b92c3"
+func hypervisorClusterDelete() {
+	taskID := "a73607cc-ae57-4efe-b573-615813e6c77d"
 	systemID := "126fd201-9e6e-5e31-9ffb-a766265b1fd3"
 	clusterID := "298a299e-78f5-5acb-86ce-4e9fdc290ab7"
 
 	gock.New("http://localhost").
-		Post("/private-cloud-business/v1beta1/systems/"+systemID+"/add-hypervisor-cluster").
+		Post("/private-cloud-business/v1beta1/systems/"+systemID+
+			"/remove-hypervisor-clusters").
+		MatchType("json").
+		JSON(map[string][]string{"hypervisor_cluster_ids": {clusterID}}).
 		Reply(202).
 		SetHeader("Location", "/data-services/v1beta1/async-operations/"+taskID)
 
@@ -28,11 +31,11 @@ func hypervisorClusterCreate() {
 		Get("/data-services/v1beta1/async-operations/"+taskID).
 		Reply(200).
 		SetHeader("Content-Type", "application/json").
-		BodyString(clusterAsync)
+		BodyString(deleteTaskOne)
 
 	gock.New("http://localhost").
-		Get("/virtualization/v1beta1/hypervisor-clusters/"+clusterID).
+		Get("/data-services/v1beta1/async-operations/"+taskID).
 		Reply(200).
 		SetHeader("Content-Type", "application/json").
-		BodyString(clusterGet)
+		BodyString(deleteTaskTwo)
 }
