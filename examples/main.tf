31c985b6bb25ea3bcbb1397bb963bea5a78d2345
sam <stuart.mclaren@hpe.com>
2024-11-28 11:52:15 +0000

    Add initial create for hypervisor server
    Note: internal/resources/server/server_resource_gen.go was
    autogenerated.
    
    This is a first step to supporting adding hypervisor servers.
    
    In thise change we:
    
     - Update the schema model for `server`.
     - Also consume the model in the server `Create` function.
    
    This is not yet complete - in particular the 'server_network'
    parameters have yet to be implemented.

--- examples/main.tf
+++ examples/main.tf
@@ -27,6 +27,9 @@ resource "hpegl_pc_hypervisor_cluster" "my_hypervisor_cluster" {
 }
 
 resource "hpegl_pc_server" "my_server" {
-        name = "16.182.105.217"
         system_id = data.hpegl_pc_system.my_system.id
+        hypervisor_cluster_id   = "acd4daea-e5e3-5f35-8be3-ce4a4b6d946c"
+        esx_root_credential_id  = "cccfcad1-85b7-4162-b16e-f7cadc2c46b5"
+        ilo_admin_credential_id = "dddfcad1-85b7-4162-b16e-f7cadc2c46b5"
+        server_network = []
 }
